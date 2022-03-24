312f0979c352d49673df44204b4648dec05044ea
21bca51 <96902342+21bca51@users.noreply.github.com>
2022-03-24 11:10:04 +0530

    This is a complete program.
    Thank you for your support to solve errors.

--- solution/sol18.py
+++ solution/sol18.py
@@ -5,12 +5,12 @@ def createList(l):
     for i in range(5):
         s=input('Enter any string : ')
         l.append(s)
-    print(l)
 createList(l)
-for i in l:
-    for j in i:
-        count=count+1
-    if(count%2==0):
-        print('The list is {} and its length is {}.'.format(l[i],count))
-    print(i)
-
+def evenlength(l):
+    for i in l:
+        count=0
+        for j in i:
+            count=count+1
+        if(count%2==0):
+            print('The list is {} and its length is {}.'.format(i,count))
+evenlength(l)
